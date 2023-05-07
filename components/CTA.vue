0081714eb06b4eb4d2da14961b749c8ce7393b72
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-05-07 20:22:17 +0600

    Added

---
+++ components/CTA.vue
@@ -0,0 +1,29 @@
+<template>
+        <div class="flex justify-center p-12 brand-color text-white">
+        <div class="grid grid-cols-2 w-4/6">
+            <div class="my-auto">
+                <p class="text-xl leading-relaxed font-light mt-4 mb-2">- LET’S CONINECT</p>
+                <h1 class="text-5xl font-bold leading-normal">Get in Touch</h1>
+                <p class="text-lg leading-relaxed font-light mt-4 mb-4">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don</p>
+            </div>
+
+            <div class="m-auto">
+                <div class="grid grid-cols-1 gap-6 mt-8 mb-6">
+                    <div>
+                        <p class="text-lg leading-relaxed">info@gmail.com</p>
+                    </div>
+
+                    <div>
+                        <p class="text-lg leading-relaxed">+8801776181910</p>
+                    </div>
+
+                    <div>
+                        <p class="text-lg leading-relaxed">+8801776181910</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
